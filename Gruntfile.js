5716c5363b5e5ebc31d60c01b76c159ad16063d1
Jan Marsch <jama@keks.com>
2014-09-18 23:43:09 +0200

    grunt & npm updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,12 +30,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', 'Development build', function() {
-    grunt.log.writeln(grunt.template.date(new Date(), 'yyyy-mm-dd HH:MM:ss'));
+    grunt.log.writeln('\033[1;36m'+ grunt.template.date(new Date(), 'yyyy-mm-dd HH:MM:ss') +'\033[0m');
     grunt.task.run('concat');
   });
 
-  grunt.registerTask('release', 'Development build', function() {
-    grunt.log.writeln(grunt.template.date(new Date(), 'yyyy-mm-dd HH:MM:ss'));
+  grunt.registerTask('release', 'Release build', function() {
+    grunt.log.writeln('\033[1;36m'+ grunt.template.date(new Date(), 'yyyy-mm-dd HH:MM:ss') +'\033[0m');
     grunt.task.run('concat');
     grunt.task.run('uglify');
   });
