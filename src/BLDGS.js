9bb97e38646d9cb9e44ae305cc09676cf2b58565
Jan Marsch <jama@keks.com>
2014-10-17 00:37:06 +0200

    Provider updates

--- src/BLDGS.js
+++ src/BLDGS.js
@@ -1,10 +1,12 @@
 
-// TODO: load tiles for a bbox
-// TODO: allow WMS calls
-
 var BLDGS = (function() {
 
   var baseURL = 'http://data.osmbuildings.org/0.2/';
+
+  var cacheData = {};
+  var cacheIndex = [];
+  var cacheSize = 0;
+  var maxCacheSize = 0;
 
 //  // http://mathiasbynens.be/notes/localstorage-pattern#comment-9
 //  var storage;
@@ -18,13 +20,13 @@ var BLDGS = (function() {
 //      };
 //    }());
 //  }
-
-  var cache = JSON.parse(storage.getItem('BLDGS') || '{}');
+//
+//  var cacheData = JSON.parse(storage.getItem('BLDGS') || '{}');
 
   function xhr(url, callback) {
-    if (cache[url]) {
+    if (cacheData[url]) {
       if (callback) {
-        callback(cache[url].json);
+        callback(cacheData[url]);
       }
       return;
     }
@@ -43,7 +45,21 @@ var BLDGS = (function() {
         try {
           json = JSON.parse(req.responseText);
         } catch(ex) {}
-        cache[url] = { json: json, time: Date.now() };
+
+        cacheData[url] = json;
+        cacheIndex.push({ url: url, size: req.responseText.length });
+        cacheSize += req.responseText.length;
+
+        while (cacheSize > maxCacheSize) {
+          var item = cacheIndex.shift();
+          cacheSize -= item.size;
+          delete cacheData[item.url];
+        }
+
+//  try {
+//    storage.setItem('BLDGS', JSON.stringify(cacheData));
+//  } catch(ex) {}
+
         callback(json);
       }
     };
@@ -54,61 +70,72 @@ var BLDGS = (function() {
     return req;
   }
 
+  function getDistance(a, b) {
+    var dx = a.x-b.x, dy = a.y-b.y;
+    return dx*dx + dy*dy;
+  }
+
   function BLDGS(options) {
     options = options || {};
-
     baseURL += (options.key || 'anonymous');
-
-    var maxAge = options.maxAge || 5*60*1000;
-
-    setInterval(function() {
-      var minTime = Date.now()-maxAge;
-      var newCacheData = {};
-      for (var key in cache) {
-        if (cache[key].time >= minTime) {
-          newCacheData[key] = cache[key];
-        }
-      }
-      cache = newCacheData;
-    }, maxAge);
+    maxCacheSize = options.cacheSize || 1024*1024; // 1MB
   };
+
+  BLDGS.TILE_SIZE = 256;
+  BLDGS.ATTRIBUTION = 'Data Service &copy; <a href="http://bld.gs">BLD.GS</a>';
 
   var proto = BLDGS.prototype;
 
   proto.getTile = function(x, y, zoom, callback) {
     var url = baseURL +'/tile/'+ zoom +'/'+ x +'/'+ y +'.json';
-    xhr(url, callback);
+    return xhr(url, callback);
   };
 
   proto.getFeature = function(id, callback) {
     var url = baseURL +'/feature/'+ id +'.json';
-    xhr(url, callback);
+    return xhr(url, callback);
   };
 
-  // TODO: optionally drop unused tiles
-  // TODO: load from center (CAM POS) out
+  proto.getBBox = function(bbox, callback) {
+    var url = baseURL +'/bbox.json?bbox='+ [bbox.n.toFixed(5),bbox.e.toFixed(5),bbox.s.toFixed(5),bbox.w.toFixed(5)].join(',');
+    return xhr(url, callback);
+  };
 
-  proto.updateBBox = function(x, y, w, h, zoom, callback) {
+  proto.getAllTiles = function(x, y, w, h, zoom, callback) {
     var
-      tileSize = 256,
+      tileSize = BLDGS.TILE_SIZE,
       fixedZoom = 16,
       realTileSize = zoom > fixedZoom ? tileSize <<(zoom-fixedZoom) : tileSize >>(fixedZoom-zoom),
       minX = x/realTileSize <<0,
       minY = y/realTileSize <<0,
       maxX = Math.ceil((x+w)/realTileSize),
       maxY = Math.ceil((y+h)/realTileSize),
-      tx, ty;
+      tx, ty,
+      queue = [];
 
     for (ty = minY; ty <= maxY; ty++) {
       for (tx = minX; tx <= maxX; tx++) {
-        this.getTile(tx, ty, fixedZoom, callback);
+        queue.push({ x:tx, y:ty, z:fixedZoom });
       }
     }
-  };
 
-//  try {
-//    storage.setItem('BLDGS', JSON.stringify(cache));
-//  } catch(ex) {}
+    var center = { x: x+(w-tileSize)/2, y: y+(h-tileSize)/2 };
+		queue.sort(function(a, b) {
+			return getDistance(a, center) - getDistance(b, center);
+		});
+
+		for (var i = 0, il = queue.length; i < il; i++) {
+      this.getTile(queue[i].x, queue[i].y, queue[i].z, callback);
+		}
+
+    return {
+      abort: function() {
+        for (var i = 0; i < queue.length; i++) {
+          queue[i].abort();
+        }
+      }
+    };
+  };
 
   return BLDGS;
 
