2b9c71235988ba36b053dbba6f502bac2df45721
Jan Marsch <jama@keks.com>
2014-12-03 22:38:53 +0100

    new build

--- dist/BLDGS.debug.js
+++ dist/BLDGS.debug.js
@@ -27,7 +27,6 @@ var cache = {
   },
 
   purge: function() {
-
     setTimeout(function() {
       while (cache.totalSize > cache.maxSize) {
         var item = cache.index.shift();
@@ -47,7 +46,9 @@ var cache = {
 function loadJSON(url, callback) {
   if (cache.has(url)) {
     if (callback) {
-      callback(cache.get(url));
+      setTimeout(function() {
+        callback(cache.get(url));
+      }, 0);
     }
     return;
   }
@@ -83,6 +84,10 @@ var baseURL = 'http://data.osmbuildings.org/0.2/';
 
 function BLDGS(options) {
   options = options || {};
+
+  this._minZoom = options.minZoom !== undefined ? options.minZoom : minZoom;
+  this._maxZoom = options.maxZoom || Infinity;
+
   baseURL += (options.key || 'anonymous');
   if (options.cacheSize !== undefined) {
     cache.setSize(options.cacheSize);
@@ -92,21 +97,30 @@ function BLDGS(options) {
 BLDGS.TILE_SIZE = 256;
 BLDGS.ATTRIBUTION = 'Data Service &copy; <a href="http://bld.gs">BLDGS</a>';
 
-var proto = BLDGS.prototype;
+BLDGS.prototype = {
+  getTile: function(x, y, zoom, callback) {
+    if (zoom < this._minZoom || zoom > this._maxZoom) {
+      setTimeout(callback, 0);
+      return { abort: function() {} };
+    }
 
-proto.getTile = function(x, y, zoom, callback) {
-  var url = baseURL +'/tile/'+ zoom +'/'+ x +'/'+ y +'.json';
-  return loadJSON(url, callback);
+    var url = baseURL +'/tile/'+ zoom +'/'+ x +'/'+ y +'.json';
+    return loadJSON(url, callback);
+  },
+
+  getFeature: function(id, callback) {
+    var url = baseURL +'/feature/'+ id +'.json';
+    return loadJSON(url, callback);
+  },
+
+  getBBox: function(n, e, s, w, callback) {
+    var url = baseURL +'/bbox.json?bbox='+ [n.toFixed(5), e.toFixed(5), s.toFixed(5), w.toFixed(5)].join(',');
+    return loadJSON(url, callback);
+  },
+
+  destroy: function() {
+    // TODO: abort requests
+    cache.clear();
+  }
 };
-
-proto.getFeature = function(id, callback) {
-  var url = baseURL +'/feature/'+ id +'.json';
-  return loadJSON(url, callback);
-};
-
-proto.getBBox = function(n, e, s, w, callback) {
-  var url = baseURL +'/bbox.json?bbox='+ [n.toFixed(5), e.toFixed(5), s.toFixed(5), w.toFixed(5)].join(',');
-  return loadJSON(url, callback);
-};
-
 return BLDGS; }(this));
